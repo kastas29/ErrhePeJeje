b10776901fcce0ac44e5926b5d4a3ca18bce1e56
kastas29 <sergi.castarnado@gmail.com>
2023-07-03 00:29:50 +0200

    Character Creation continued. Still work to do

--- Assets/CharacterCustomizationController.cs
+++ Assets/CharacterCustomizationController.cs
@@ -6,35 +6,27 @@ using UnityEngine.UI;
 public class CharacterCustomizationController : MonoBehaviour
 {
 
-    public List<Sprite> FacesImages;
-    public List<Sprite> HeadImages;
-    public List<Sprite> BaseImages;
+    public List<Sprite> BodyImages;
+    public List<Sprite> PantsImages;
+    public List<Sprite> EyesImages;
+    public List<Sprite> HeadDecorationsImages;
+    public List<Sprite> FaceDecorationsImages;
 
-    public int FaceIndex = 0;
-    public int HeadIndex = 0;
-    public int BaseIndex = 0;
+    public List<GameObject> Players;
 
-    public Image FaceImage;
-    public Image HeadImage;
-    public Image BaseImage;
-
-    public void PreviousFace()
+    public void GenerateNewPlayers()
     {
-        FaceIndex--;
-        if (FaceIndex < 0)
+        print("start generate");
+        foreach (GameObject Character in Players)
         {
-            FaceIndex = FacesImages.Count - 1;
+            print("character name " + Character.name);
+            
+            Character.transform.GetChild(1).transform.GetComponent<Image>().sprite = BodyImages[Random.Range(0, BodyImages.Count - 1)];
+            Character.transform.GetChild(2).transform.GetComponent<Image>().sprite = PantsImages[Random.Range(0, PantsImages.Count - 1)];
+            Character.transform.GetChild(3).transform.GetComponent<Image>().sprite = EyesImages[Random.Range(0, EyesImages.Count - 1)];
+            Character.transform.GetChild(4).transform.GetComponent<Image>().sprite = HeadDecorationsImages[Random.Range(0, HeadDecorationsImages.Count - 1)];
+            Character.transform.GetChild(5).transform.GetComponent<Image>().sprite = FaceDecorationsImages[Random.Range(0, FaceDecorationsImages.Count - 1)];
         }
-        FaceImage.sprite = FacesImages[FaceIndex];
-    }
-
-    public void NextFace()
-    {
-        FaceIndex++;
-        if (FaceIndex > FacesImages.Count - 1)
-        {
-            FaceIndex = 0;
-        }
-        FaceImage.sprite = FacesImages[FaceIndex];
+        print("end generate");
     }
 }
