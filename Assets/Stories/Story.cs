0b266a9a379070f1406db1ee583b456ee137ff75
kastas29 <sergi.castarnado@gmail.com>
2023-07-19 22:54:02 +0200

    me gusta el kebab a las once de la noche

---
+++ Assets/Stories/Story.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+
+public class Story
+{
+    public Image StoryIcon;
+    public string StoryName;
+    public string StoryDescription;
+    public bool IsSelected;
+}
---
+++ Assets/Stories/StoryData.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryData : MonoBehaviour
+{
+    public List<Story> stories = new List<Story>();
+}
